9bc684c4b0052d580c951354b6df8ca4b1e00ff0
Parker Mathewson <parkerfm34@gmail.com>
2014-05-12 22:06:57 -0700

    all done

--- final.js
+++ final.js
@@ -1,23 +1,77 @@
-//var bikes = ['furtado','joplin','juno','nevis','origin'];
-// MAKE THIS ARRAY DYNAMIC BY PULLING THE NAMES FROM THE DB
+var bikes = [];
+
+// Creates the dynamic array once the page is populated from the db
+function initBike() {
+	$(".modal").each(function(){
+		if(this.id != "rider")
+			bikes.push( this.id );
+	});
+}
+
 var currentBike = "";
-$(".modal").click(function(){
-	currentBike = $(this).attr('id');
-	changeImage();
+
+// Dynamic code from php does not like the $(".modal").click code, therefore
+// we must use the document and on method from javascript
+$(function() {
+	$(document).on('click', '.modal', function(){
+		currentBike = $(this).attr('id');
+		changeImage();
+	});
 });
-			
+
 function shut() {
-	
+	$("#nav").removeClass("front");
+	$("#lightbox").css("left", "-2000px");
+	$(".modal").css("left", "0px");
 }
 			
 function prev() {
-
+	var count = 0;
+	for(count = 0; count < 5; count++)
+	{
+		if(bikes[count] == currentBike)
+		{
+			if(count == 0)
+			{
+				count = 4;
+				currentBike = bikes[count];
+			}
+			else
+				currentBike = bikes[count-1];
+			changeImage();
+			break;
+		}
+	}
+	if(count == 5)
+		currentBike = bikes[4];
+	changeImage();
 }
 			
 function next() {
-
+	var count = 0;
+	for(count = 0; count < 5; count++)
+	{
+		if(bikes[count] == currentBike)
+		{
+			if(count == 4)
+			{
+				count = 0;
+				currentBike = bikes[count];
+			}
+			else
+				currentBike = bikes[count+1];
+			changeImage();
+			break;
+		}
+	}
+	if(count == 5)
+		currentBike = bikes[0];
+	changeImage();
 }
 			
 function changeImage() {
-
+	$(".modal").css("left", "-2000px");
+	$("#lightbox").css("background-image", "url('images/" + currentBike + ".jpg')");
+	$("#lightbox").css("left", "0px");
+	$("#nav").addClass("front");
 }
--- final.php
+++ final.php
@@ -1,6 +1,5 @@
 <?php 
 	$title = "Final of 337";
-	$DBH = new PDO("mysql:host=localhost;dbname=final", 'root', '');
 ?>
 
 
@@ -11,12 +10,12 @@
 		<meta name="author" content="Parker Mathewson" />
 		<meta name="description" content="Our final exam" />
 		<link href="final.css" rel="stylesheet" type="text/css">
-		<script src="jquery-2.1.0.min.js"></script>
+		<script src="jquery-2.1.0-min.js"></script>
 		<script src="final.js"></script>
 		
 		<!-- Date: 2014-05-12 -->
 	</head>
-
+	
 
 	<section class="wrapper">
 		<div id="lightbox"> </div>
@@ -41,15 +40,35 @@
 ?>
 
 <?php
-/* 
-		<span> </span>
-		<span>Furtado</span>
-		<span>Joplin</span>
-		<span>Juno</span>
-		<span>Nevis</span>
-		<span>Origin</span>
-*/ 
+	$DBH = new PDO("mysql:host=localhost;dbname=final", 'root', '');
+	$stmt = $DBH->prepare("SELECT * FROM product");
+	$stmt->execute(); 
+	$products = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+		foreach($products as $product) {
+			// Check for blank title, if it is blank, it is rider
+			if(!empty($product['title']))
+			{
+				// Just using the straight echo would extract my forward slashes '/' so I put the html into a string
+				// which printed it out just as it was called.
+				$str = '<div class="modal" id="' . $product['title'] . '" style="background-image: url(' . $product["imagesmall"] . ');">';
+				echo $str;
+				echo '<span>' . $product['title'] . ' </span>';
+				echo '</div>';
+			}
+			else
+			{
+				$str = '<div class="modal" id="rider" style="background-image: url(images/rider-small.jpg);">';
+				echo $str;
+				echo '<span> </span>';
+				echo '</div>';
+			}
+		}
 ?>
+	<script>
+		// Initializes the array of bikes for the scrolling pictures
+		initBike();
+	</script>
 	</section>
 
 </html>
